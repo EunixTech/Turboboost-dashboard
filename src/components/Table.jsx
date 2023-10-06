f7abbdba42d8fc0218bbb79afce6c438d7fa87a3
Rishisippy <rishi@eunix.tech>
2023-10-06 17:07:14 +0530

    using css varible and better naming convention

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -4,57 +4,55 @@ import TableItem from "./TableItem";
 import { useSelector } from "react-redux";
 
 const Table = ({ setSelected1 }) => {
-    const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  
-    const [selected, setSelected] = useState([]);
-    const dark = useSelector((state) => state.home.dark);
-    return (
-      <div
-        style={{
-          borderColor: dark ? "#1F2329" : "#ebebeb",
-          color: dark ? "#fff" : "#000",
-        }}
-        className="w-[100%] border-t-[1px]  border-[#ebebeb] mt-[10px] mobile:pb-[10px] laptop:pb-[0] overflow-x-auto overflow-y-hidden scroll-x-cool"
-      >
-        <div className="mobile:w-[1200px] laptop:w-[100%]">
-          <TableHeader
-            change={() => {
-              if (selected.length !== arr.length) {
-                const arrr = [];
-                for (let i = 0; i < arr.length; i++) {
-                  arrr.push(i);
-                }
-                setSelected(arrr);
-                setSelected1(arrr);
-              } else {
-                setSelected([]);
-                setSelected1([]);
+  // Use a more descriptive name for your array
+  const tableData = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  const [selected, setSelected] = useState([]);
+  const dark = useSelector((state) => state.home.dark);
+  const wrapperClassName = dark ? "divWrapperDarkMode" : "divWrapper";
+
+  return (
+    <div
+      className={`w-[100%] border-t-[1px] border-[#ebebeb] mt-[10px] mobile:pb-[10px] laptop:pb-[0] overflow-x-auto overflow-y-hidden scroll-x-cool${wrapperClassName}`}
+    >
+      <div className="mobile:w-[1200px] laptop:w-[100%]">
+        <TableHeader
+          change={() => {
+            if (selected.length !== tableData.length) {
+              const newArr = [];
+              for (let i = 0; i < tableData.length; i++) {
+                newArr.push(i);
               }
-            }}
-          />
-          {arr.map((item, i) => {
-            return (
-              <TableItem
-                key={i}
-                selected={selected.includes(i)}
-                change={() => {
-                  if (selected.includes(i)) {
-                    const finale = selected.filter((e) => e !== i);
-                    setSelected(finale);
-                    setSelected1(finale);
-                  } else {
-                    setSelected([...selected, i]);
-                    setSelected1([...selected, i]);
-                  }
-                }}
-                last={i === arr.length - 1}
-              />
-            );
-          })}
-        </div>
+              setSelected(newArr);
+              setSelected1(newArr);
+            } else {
+              setSelected([]);
+              setSelected1([]);
+            }
+          }}
+        />
+        {tableData.map((item, i) => {
+          return (
+            <TableItem
+              key={i}
+              selected={selected.includes(i)}
+              change={() => {
+                if (selected.includes(i)) {
+                  const updatedSelection = selected.filter((e) => e !== i);
+                  setSelected(updatedSelection);
+                  setSelected1(updatedSelection);
+                } else {
+                  setSelected([...selected, i]);
+                  setSelected1([...selected, i]);
+                }
+              }}
+              last={i === tableData.length - 1}
+            />
+          );
+        })}
       </div>
-    );
-  };
-  
+    </div>
+  );
+};
 
 export default Table;
