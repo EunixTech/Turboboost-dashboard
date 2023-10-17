96371ccee9361471e1e754e10b1cf3ec9b84170d
Rishisippy <rishi@eunix.tech>
2023-10-17 15:18:56 +0530

    git push origin feature/login-page

--- src/services/store.jsx
+++ src/services/store.jsx
@@ -3,12 +3,13 @@ import homeReducer from "./home";
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from '../slice/authSlice';
 import { apiSlice } from '../slice/apiSlice';
-
+import {loginWithGoogle } from "../slice/userLoginWithGoogleSlice"
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
-    home: homeReducer 
+    home: homeReducer,
+    loginGoogle: loginWithGoogle, 
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
