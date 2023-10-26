bee266f2aa8d6077059668358a34291309f8db82
themanmohan <gitmail223344@gmail.com>
2023-10-26 20:19:19 +0530

    refactor: removing inline css and unused code

--- src/views/DashboardPage.jsx
+++ src/views/DashboardPage.jsx
@@ -31,14 +31,14 @@ const DashboardPage = () => {
         <div className="w-[100%] h-[100vh] overflow-hidden flex flex-col">
             <div className="w-[100%] h-[50px] shrink-0"></div>
 
-            <div style={{ backgroundColor: dark ? "#09090b" : "#FAFAFC" }} className="w-[100%] h-[100%] flex flex-col items-center  overflow-y-auto scroll-bar-cool111 overflow-x-hidden laptop:px-[20px]  desktop:px-[80px]">
+            <div className={`${dark ? "backgroundDarkMode" : "background"} w-[100%] h-[100%] flex flex-col items-center  overflow-y-auto scroll-bar-cool111 overflow-x-hidden laptop:px-[20px]  desktop:px-[80px]`}>
 
                 <div className="w-[100%] pb-[50px] max-w-[1920px] min-h-[100vh]">
 
                     <div className="w-[100%] pt-[50px] h-[40px] mobile:px-[10px] flex items-center justify-between">
 
                         <div className="flex items-center mb-[20px] justify-center">
-                            <h1 style={{ color: dark ? "#fff" : "#000" }} className="text-[18px] f2 font-medium" > Good evening, Kyle! </h1>
+                            <h1 className={`${dark ? "headingDarkMode" : "heading"} text-[18px] f2 font-medium`} > Good evening, Kyle! </h1>
 
                             <img
                                 src="/graphic/dashboard/hifi.png"
@@ -53,7 +53,7 @@ const DashboardPage = () => {
                                     <div className="w-[10px] h-[10px] rounded-[50%] bg-[#38F8AC]"></div>
                                 </div>
 
-                                <h1 style={{ color: dark ? "#fff" : "#000" }} className=" ml-[10px] f2 laptop:text-[16px] desktop:text-[18px] font-medium" > TurboBoost Service Status </h1>
+                                <h1 className={`${dark ? "headingDarkMode" : "heading"} ml-[10px] f2 laptop:text-[16px] desktop:text-[18px] font-medium`} > TurboBoost Service Status </h1>
                             </div>
                         )}
                     </div>
@@ -77,8 +77,8 @@ const DashboardPage = () => {
                     <div className="w-[100%] px-[10px]">
                         <div style={{ backgroundColor: dark ? "#111317" : "#fff", borderColor: dark ? "#1F2329" : "#ebebeb" }} className="w-[100%]  px-[15px] py-[15px] min-h-[110px] bg-[#fff] border-[1px] border-[#EBEBEB] rounded-[10px] mt-[24px]">
 
-                            <h1 style={{ color: dark ? "#fff" : "#000" }} className="desktop:text-[20px] f2 laptop:text-[20px] leading-[22px] font-semibold" >Service Usage Log </h1>
-                            <p style={{ color: dark ? "#ffffff74" : "#0a0a187e" }} className="text-[#0a0a187e] f2 laptop:text-[14px] desktop:text-[14px] font-medium tracking-wide" > This Month </p>
+                            <h1 className={`${dark ? "headingDarkMode" : "heading"} desktop:text-[20px] f2 laptop:text-[20px] leading-[22px] font-semibold`} >Service Usage Log </h1>
+                            <p className={`${dark ? "subHeadingDarkMode" : "subHeading"} text-[#0a0a187e] f2 laptop:text-[14px] desktop:text-[14px] font-medium tracking-wide`} > This Month </p>
                             <DomLineChart className="custom-chart" />
 
                         </div>
@@ -86,21 +86,11 @@ const DashboardPage = () => {
 
                     <div className="w-[100%] mt-[24px] mobile:px-[10px] desktop:grid  desktop:grid-cols-3 laptop:grid-cols-2 gap-x-[24px] gap-y-[10px]">
 
-                        <div style={{
-                            backgroundColor: dark ? "#111317" : "#fff",
-                            borderColor: dark ? "#1F2329" : "#ebebeb",
-                        }}
-                            className=" h-[100%] bg-[#fff] mobile:mb-[10px] laptop:mb-[0] border-[1px] px-[15px] py-[14px] border-[#EBEBEB]  rounded-[8px]"
-                        >
+                        <div className={`${dark ? "divWrapperDarkMode" : "divWrapper"} h-[100%] bg-[#fff] mobile:mb-[10px] laptop:mb-[0] border-[1px] px-[15px] py-[14px] border-[#EBEBEB]  rounded-[8px]`}>
                             <div className="w-[100%]  flex items-center justify-between">
-                                <p style={{ color: dark ? "#fff" : "#000" }} className="text-[15px] f2 translate-y-[0px] font-semibold tracking-wide"> Google Page Score </p>
+                                <p className={`${dark ? "headingDarkMode" : "heading"} text-[15px] f2 translate-y-[0px] font-semibold tracking-wide`}> Google Page Score </p>
 
-                                <div style={{
-                                    backgroundColor: dark ? "#111317" : "#fff",
-                                    borderColor: dark ? "#1F2329" : "#ebebeb",
-                                }}
-                                    className="w-[180px] cursor-pointer  overflow-hidden border-[1px] h-[30px] flex rounded-[7px] items-center justify-center"
-                                >
+                                <div className={`${dark ? "divWrapperDarkMode" : "divWrapper"} w-[180px] cursor-pointer  overflow-hidden border-[1px] h-[30px] flex rounded-[7px] items-center justify-center`}>
                                     <div
                                         onClick={() => { setVitsals(true) }}
                                         style={{
@@ -146,7 +136,7 @@ const DashboardPage = () => {
 
                             {!coreVitals ? (
                                 <>
-                                    <p style={{ color: dark ? "#ffffff74" : "#0a0a187e" }} className="text-[12px]  font-semibold f2"> Performance </p>
+                                    <p className={`${dark ? "subHeadingDarkMode" : "subHeading"} text-[12px]  font-semibold f2`}> Performance </p>
 
                                     <div className="flex items-center  justify-around h-[140px]">
                                         {progressBarDataArr.length && progressBarDataArr.map((item, index) => (
@@ -164,13 +154,13 @@ const DashboardPage = () => {
 
                         </div>
 
-                        <div style={{ backgroundColor: dark ? "#111317" : "#fff", borderColor: dark ? "#1F2329" : "#ebebeb", }} className=" h-[100%] mobile:mb-[10px] laptop:mb-[0]  bg-[#fff] border-[1px] px-[15px] py-[14px] border-[#EBEBEB] rounded-[8px]">
+                        <div className={`${dark ? "divWrapperDarkMode" : "divWrapper"} h-[100%] mobile:mb-[10px] laptop:mb-[0]  bg-[#fff] border-[1px] px-[15px] py-[14px] border-[#EBEBEB] rounded-[8px]`}>
                             <div className="w-[100%]  flex items-center justify-between">
-                                <p style={{ color: dark ? "#fff" : "#000" }} className="text-[15px] f2 translate-y-[0px] font-semibold tracking-wide" > Total Cache Status </p>
+                                <p className={`${dark ? "headingDarkMode" : "heading"} text-[15px] f2 translate-y-[0px] font-semibold tracking-wide`} > Total Cache Status </p>
 
-                                <div style={{ color: dark ? "#ffffff74" : "#0a0a187e" }}
-                                    className={`text-[#0a0a187e] f2 ${dark ? "text-[#ffffff74]  hover:bg-[#ffffff30]" : "text-[#0a0a187e] hover:bg-[#e1e1e1]"} px-[7px] py-[2px] rounded-sm cursor-pointer text-[13px] translate-y-[1px] font-medium `}
-                                > View Details </div>
+                                <div className={`${dark ? "subHeadingDarkMode" : "subHeading"} text-[#0a0a187e] f2 ${dark ? "text-[#ffffff74]  hover:bg-[#ffffff30]" : "text-[#0a0a187e] hover:bg-[#e1e1e1]"} px-[7px] py-[2px] rounded-sm cursor-pointer text-[13px] translate-y-[1px] font-medium `}>
+                                    View Details
+                                </div>
                             </div>
 
                             <div className="w-[100%] justify-center items-center flex h-[130px] mt-[25px]">
@@ -191,14 +181,13 @@ const DashboardPage = () => {
                             </div>
                         </div>
 
-                        <div style={{ backgroundColor: dark ? "#111317" : "#fff", borderColor: dark ? "#1F2329" : "#ebebeb" }} className=" relative mobile:mb-[10px] laptop:mb-[0]   bg-[#fff] border-[1px]  py-[14px] border-[#EBEBEB] rounded-[8px]">
+                        <div className={`${dark ? "divWrapperDarkMode" : "divWrapper"}  relative mobile:mb-[10px] laptop:mb-[0]   bg-[#fff] border-[1px]  py-[14px] border-[#EBEBEB] rounded-[8px]`}>
                             <div className="w-[100%] px-[15px] mb-[10px] flex items-center justify-between">
+                                <p className={`${dark ? "headingDarkMode" : "heading"} text-[15px] f2 translate-y-[0px] font-medium tracking-wide`} > Quick Actions </p>
 
-                                <p style={{ color: dark ? "#fff" : "#000" }} className="text-[15px] f2 translate-y-[0px] font-medium tracking-wide"> Quick Actions </p>
-
-                                <div style={{ color: dark ? "#ffffff74" : "#0a0a187e" }}
+                                <div
                                     onClick={() => { router("/settings") }}
-                                    className={`text-[#0a0a187e] f2 ${dark ? "text-[#ffffff74]  hover:bg-[#ffffff30]" : "text-[#0a0a187e] hover:bg-[#e1e1e1]"}  px-[7px] py-[2px] rounded-sm cursor-pointer text-[13px] translate-y-[1px] font-medium`}
+                                    className={`${dark ? "subHeadingDarkMode" : "subHeading"} text-[#0a0a187e] f2 ${dark ? "text-[#ffffff74]  hover:bg-[#ffffff30]" : "text-[#0a0a187e] hover:bg-[#e1e1e1]"}  px-[7px] py-[2px] rounded-sm cursor-pointer text-[13px] translate-y-[1px] font-medium`}
                                 >
                                     All Settings
                                 </div>
