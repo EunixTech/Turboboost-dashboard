ffea32ac8ebf1ad06a7612ad3c96299d29977f24
themanmohan <gitmail223344@gmail.com>
2023-12-18 23:18:47 +0530

    fix: fixing intercom issue

---
+++ src/views/404.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function NotFound() {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          textAlign: "center",
+          marginTop: "50px",
+        }}
+      >
+        <img
+          src="/graphic/notfound/notfound.svg"
+          className="w-[645px] h-[382px]  shrink-0"
+          alt=""
+        />
+      </div>
+
+      <p className="notfound-txt">Looks like you typed too fast...</p>
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <button className="return-btn">Return to home page</button>
+      </div>
+    </>
+  );
+}
