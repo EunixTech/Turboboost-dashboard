76d8abf05069780c28c8941b18f1f1830b3aa996
Rishisippy <rishi@eunix.tech>
2023-12-18 15:36:03 +0530

    added taost and validation

--- src/layouts/index/sidebar/mobile.jsx
+++ src/layouts/index/sidebar/mobile.jsx
@@ -90,27 +90,27 @@ const Sidebar = ({ transition, cancel }) => {
           </div>
           <div className="w-[100%] mt-[20px]">
             <Item title={"Dashboard"} route="/dashboard" src="/icon1.svg" />
-            <Item
+            {/* <Item
               title={"Connect Website"}
               route="/connect-website"
               src="/icon2.svg"
-            />
-            <Item
+            /> */}
+            {/* <Item
               title={"Cache Warmup"}
               route="/cache-warmup"
               src="/icon3.svg"
-            />
-            <Item
+            /> */}
+            {/* <Item
               title={"Cache Statussss"}
               route="/cache-status"
               src="/icon4.svg"
-            />
-            <Item title={"Logs"} route="/logs" src="/icon5.svg" />
-            <Item
+            /> */}
+            {/* <Item title={"Logs"} route="/logs" src="/icon5.svg" /> */}
+            {/* <Item
               title={"Integrations"}
               route="/integrations"
               src="/icon6.svg"
-            />
+            /> */}
             <Item title={"Billing"} route="/billing" src="/icon7.svg" />
             <Item title={"Settings"} route="/settings" src="/icon8.svg" />
           </div>
