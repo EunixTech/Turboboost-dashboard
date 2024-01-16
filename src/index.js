4eaa5eb76a383b57f40dc01c94d05e5342634c7b
themanmohan <gitmail223344@gmail.com>
2024-01-16 20:28:21 +0530

    fixing assets optimization issue

--- src/index.js
+++ src/index.js
@@ -116,8 +116,24 @@ const App = () => {
     // } else {
     //   setShowOnboardingModal(!!userToken);
     // }
+
+    const checkAuth = () => {
+      const authToken = localStorage.getItem('authToken');
+      const isLoginRoute = window.location.pathname === '/login-shopify';
+
+      if (!authToken && !isLoginRoute) {
+        // Redirect to login page if authToken is not available and not on the login route
+        window.location.replace('/login-shopify');
+      } else if (authToken && isLoginRoute) {
+        // Redirect to dashboard page if authToken is available and on the login route
+        window.location.replace('/dashboard');
+      }
+    };
+
+    checkAuth();
  
   }, []);
+
 
   console.log("workingworking***", cookies)
   return (
