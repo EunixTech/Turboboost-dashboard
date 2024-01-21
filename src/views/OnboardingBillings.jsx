12da9b16f387d45e4f7c068be83d075e0c74bc61
themanmohan <gitmail223344@gmail.com>
2024-01-21 19:02:38 +0530

    checking user subscription detail

--- src/views/OnboardingBillings.jsx
+++ src/views/OnboardingBillings.jsx
@@ -99,185 +99,189 @@ const OnboardingBillings = () => {
     <div className="overflow-hidden flex flex-col items-center justify-center">
       <TitleManager title="Onboarding" conicalURL="onboarding" />
       <div className="w-full max-w-screen-xl"></div>
-
-      <div
+{
+   loader ?
+    <AnimatedLoader /> :
+    <div
+    style={{
+      backgroundColor: dark ? "#fff" : "#000",
+      // height: '100vh',
+      width: "100%",
+      overflowY: "auto",
+    }}
+    className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center billing-modal-background bg-[#FAFAFC] mobile:px-[20px] rounded-md relative"
+  >
+    x1
+    <div className="max-w-[1200px] w-full mx-auto h-[85vh]" >
+      <h1
         style={{
-          backgroundColor: dark ? "#fff" : "#000",
-          // height: '100vh',
-          width: "100%",
-          overflowY: "auto",
+          color: dark ? "#fff" : "#fff",
         }}
-        className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center billing-modal-background bg-[#FAFAFC] mobile:px-[20px] rounded-md relative"
+        className="text-[20px] font-bold tracking-wide text-center pt-[30px]"
       >
-        x1
-        <div className="max-w-[1200px] w-full mx-auto h-[85vh]" >
-          <h1
-            style={{
-              color: dark ? "#fff" : "#fff",
+        Select A Plan{" "}
+      </h1>
+      <h1 className="text-[14px] font-medium text-[#696e7ea8] tracking-wide text-center mt-[2px]">
+        A faster website is just a click away!{" "}
+      </h1>
+      <div className="w-[100%] mt-[15px] flex items-center justify-center">
+        <div
+          style={
+            {
+              // backgroundColor: dark ? "#12122B" : "#12122B",
+              // borderColor: dark ? "#1F2329" : "#ebebeb",
+            }
+          }
+          className="flex w-[200px] h-[40px]  border-[1px]   rounded-[4px] px-[3px] py-[3px]"
+        >
+          <div
+            onClick={() => {
+              setSelected(0);
             }}
-            className="text-[20px] font-bold tracking-wide text-center pt-[30px]"
+            style={{
+              // backgroundColor: selected === 0 ? "#18df903f" : "",
+              color: selected === 0 ? "#0FE38F" : "#85858C",
+            }}
+            className="w-[50%] cursor-pointer h-[100%] rounded-[4px] text-[14px] font-medium flex items-center justify-center tracking-wide"
           >
-            Select A Plan{" "}
-          </h1>
-          <h1 className="text-[14px] font-medium text-[#696e7ea8] tracking-wide text-center mt-[2px]">
-            A faster website is just a click away!{" "}
-          </h1>
-          <div className="w-[100%] mt-[15px] flex items-center justify-center">
-            <div
-              style={
-                {
-                  // backgroundColor: dark ? "#12122B" : "#12122B",
-                  // borderColor: dark ? "#1F2329" : "#ebebeb",
-                }
-              }
-              className="flex w-[200px] h-[40px]  border-[1px]   rounded-[4px] px-[3px] py-[3px]"
-            >
-              <div
-                onClick={() => {
-                  setSelected(0);
-                }}
-                style={{
-                  // backgroundColor: selected === 0 ? "#18df903f" : "",
-                  color: selected === 0 ? "#0FE38F" : "#85858C",
-                }}
-                className="w-[50%] cursor-pointer h-[100%] rounded-[4px] text-[14px] font-medium flex items-center justify-center tracking-wide"
-              >
-                Monthly
-              </div>
-              <div
-                onClick={() => {
-                  setSelected(1);
-                }}
-                style={{
-                  backgroundColor: selected === 1 ? "#18df903f" : "",
-                  color: selected === 1 ? "#0FE38F" : "#85858C",
-                }}
-                className="w-[50%] cursor-pointer h-[100%] rounded-[4px] text-[14px] font-medium flex items-center justify-center tracking-wide"
-              >
-                Annually
-              </div>
-            </div>
-            <img
-              src="/AnualArrow.svg"
-              className="ml-[1px] annualArrow"
-              alt=""
-            />
+            Monthly
           </div>
-          <p className="annualText">2 months free!</p>
-          
-          <div className="w-[100%] mt-[20px] gap-[20px] grid laptop:grid-cols-3">
-            {planMockData.slice(0, 3).map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  style={{
-                    backgroundColor: dark ? "#111317" : "#fff",
-                    borderColor: dark ? "#1F2329" : "#ebebeb",
-                    width: "100%",
-                  }}
-                  className="bg-[#fff]  border-[1px] border-[#EBEBEB] py-[12px] mobile:mb-[3px] laptop:mb-[30px] rounded-[8px]"
-                >
-                  <h1
-                    style={{
-                      color: dark ? "#fff" : "#000",
-                    }}
-                    className="text-[20px]  px-[17px] font-bold tracking-wide"
-                  >
-                    {item?.name}
-                  </h1>
-                  <p
-                    style={{
-                      color: dark ? "#ffffff74" : "#0a0a187e",
-                    }}
-                    className="text-[14px] h-[42px] px-[17px] text-[#0a0a187a] font-medium tracking-wide"
-                  >
-                    {item?.desc}
-                  </p>
-                  <div
-                    style={{
-                      color: dark ? "#fff" : "#000",
-                    }}
-                    className="w-[100%] leading-[32px] px-[17px] relative mt-[10px] shrink-0 text-[32px] font-bold "
-                  >
-                    {selected === 0
-                      ? `$ ${item?.monthlyPrice}`
-                      : `$ ${item?.annuallyPrice}`}
-                    <span className="text-[14px] font-medium text-[#696e7e89]">
-                      {" "}
-                      /month
-                    </span>
-                  </div>
-                  <p
-                    style={{
-                      color: dark ? "#ffffff74" : "#0a0a187e",
-                    }}
-                    className="text-[14px]  px-[17px] mt-[14px] text-[#0a0a187a]  tracking-wide"
-                  >
-                    <span className="font-bold">{item?.pageViews}</span> page
-                    views/mo
-                  </p>
-                  <p
-                    style={{
-                      color: dark ? "#ffffff74" : "#0a0a187e",
-                    }}
-                    className="text-[14px]  px-[17px] text-[#0a0a187a]  tracking-wide"
-                  >
-                    {/* <span className="font-bold">{item?.CDN_bandWidth}</span> CDN
-                    bandwidth/mo */}
-                  </p>
-                  <div className="w-[100%] px-[17px] mt-[15px]">
-                    <div
-                      onClick={() => {
-                        handleBilling(item);
-                      }}
-                      style={{
-                        borderColor: dark ? "#1F2329" : "#ebebeb",
-                      }}
-                      className={`w-[100%] h-[38px] text-[${
-                        dark ? "#fff" : "#000"
-                      }] hover:bg-[#38F8AC] hover:text-[#000] cursor-pointer rounded-[3px] border-[1px] border-[#ebebeb] text-[14px] font-bold text-[#000] tracking-wide flex items-center justify-center`}
-                    >
-                      Start Free Trail
-                    </div>
-                  </div>
-                  <div className="w-[100%]  px-[17px] mt-[15px] pt-[15px] border-t-[1px] border-[#ebebeb]">
-                    <p
-                      style={{
-                        color: dark ? "#fff" : "#000",
-                      }}
-                      className="text-[14px] font-bold tracking-wide"
-                    >
-                      {item?.name} Plan Includes
-                    </p>
-                    {item?.includes?.map((includesItems, includesIndex) => {
-                      return (
-                        <div
-                          className="w-[100%] mt-[10px] flex justify-between"
-                          key={includesIndex}
-                        >
-                          <img
-                            src="/graphic/status/check.svg"
-                            className="w-[13px] mr-[10px] shrink-0"
-                            alt=""
-                          />
-                          <p
-                            style={{
-                              color: dark ? "#ffffff74" : "#0a0a187e",
-                            }}
-                            className="text-[14px] w-[100%] text-[#696e7e89] tracking-wide font-medium "
-                          >
-                            {includesItems}
-                          </p>
-                        </div>
-                      );
-                    })}
-                  </div>
-                </div>
-              );
-            })}
+          <div
+            onClick={() => {
+              setSelected(1);
+            }}
+            style={{
+              backgroundColor: selected === 1 ? "#18df903f" : "",
+              color: selected === 1 ? "#0FE38F" : "#85858C",
+            }}
+            className="w-[50%] cursor-pointer h-[100%] rounded-[4px] text-[14px] font-medium flex items-center justify-center tracking-wide"
+          >
+            Annually
           </div>
         </div>
+        <img
+          src="/AnualArrow.svg"
+          className="ml-[1px] annualArrow"
+          alt=""
+        />
       </div>
+      <p className="annualText">2 months free!</p>
+      
+      <div className="w-[100%] mt-[20px] gap-[20px] grid laptop:grid-cols-3">
+        {planMockData.slice(0, 3).map((item, index) => {
+          return (
+            <div
+              key={index}
+              style={{
+                backgroundColor: dark ? "#111317" : "#fff",
+                borderColor: dark ? "#1F2329" : "#ebebeb",
+                width: "100%",
+              }}
+              className="bg-[#fff]  border-[1px] border-[#EBEBEB] py-[12px] mobile:mb-[3px] laptop:mb-[30px] rounded-[8px]"
+            >
+              <h1
+                style={{
+                  color: dark ? "#fff" : "#000",
+                }}
+                className="text-[20px]  px-[17px] font-bold tracking-wide"
+              >
+                {item?.name}
+              </h1>
+              <p
+                style={{
+                  color: dark ? "#ffffff74" : "#0a0a187e",
+                }}
+                className="text-[14px] h-[42px] px-[17px] text-[#0a0a187a] font-medium tracking-wide"
+              >
+                {item?.desc}
+              </p>
+              <div
+                style={{
+                  color: dark ? "#fff" : "#000",
+                }}
+                className="w-[100%] leading-[32px] px-[17px] relative mt-[10px] shrink-0 text-[32px] font-bold "
+              >
+                {selected === 0
+                  ? `$ ${item?.monthlyPrice}`
+                  : `$ ${item?.annuallyPrice}`}
+                <span className="text-[14px] font-medium text-[#696e7e89]">
+                  {" "}
+                  /month
+                </span>
+              </div>
+              <p
+                style={{
+                  color: dark ? "#ffffff74" : "#0a0a187e",
+                }}
+                className="text-[14px]  px-[17px] mt-[14px] text-[#0a0a187a]  tracking-wide"
+              >
+                <span className="font-bold">{item?.pageViews}</span> page
+                views/mo
+              </p>
+              <p
+                style={{
+                  color: dark ? "#ffffff74" : "#0a0a187e",
+                }}
+                className="text-[14px]  px-[17px] text-[#0a0a187a]  tracking-wide"
+              >
+                {/* <span className="font-bold">{item?.CDN_bandWidth}</span> CDN
+                bandwidth/mo */}
+              </p>
+              <div className="w-[100%] px-[17px] mt-[15px]">
+                <div
+                  onClick={() => {
+                    handleBilling(item);
+                  }}
+                  style={{
+                    borderColor: dark ? "#1F2329" : "#ebebeb",
+                  }}
+                  className={`w-[100%] h-[38px] text-[${
+                    dark ? "#fff" : "#000"
+                  }] hover:bg-[#38F8AC] hover:text-[#000] cursor-pointer rounded-[3px] border-[1px] border-[#ebebeb] text-[14px] font-bold text-[#000] tracking-wide flex items-center justify-center`}
+                >
+                  Start Free Trail
+                </div>
+              </div>
+              <div className="w-[100%]  px-[17px] mt-[15px] pt-[15px] border-t-[1px] border-[#ebebeb]">
+                <p
+                  style={{
+                    color: dark ? "#fff" : "#000",
+                  }}
+                  className="text-[14px] font-bold tracking-wide"
+                >
+                  {item?.name} Plan Includes
+                </p>
+                {item?.includes?.map((includesItems, includesIndex) => {
+                  return (
+                    <div
+                      className="w-[100%] mt-[10px] flex justify-between"
+                      key={includesIndex}
+                    >
+                      <img
+                        src="/graphic/status/check.svg"
+                        className="w-[13px] mr-[10px] shrink-0"
+                        alt=""
+                      />
+                      <p
+                        style={{
+                          color: dark ? "#ffffff74" : "#0a0a187e",
+                        }}
+                        className="text-[14px] w-[100%] text-[#696e7e89] tracking-wide font-medium "
+                      >
+                        {includesItems}
+                      </p>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  </div>
+}
+     
     </div>
   );
 };
