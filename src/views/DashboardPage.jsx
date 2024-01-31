4e5767d59b86a53064820be47f81c0df26b1093d
themanmohan <gitmail223344@gmail.com>
2024-01-31 22:07:10 +0530

    feat: Adding percentage loader

--- src/views/DashboardPage.jsx
+++ src/views/DashboardPage.jsx
@@ -421,7 +421,11 @@ const Dashboard = () => {
 
   return (loadingAPI || loader || loading) ? 
     (() => {
-      if ((!dashboardOptimizationValue && !userToken1)) {
+      if (!userToken1)  {
+        return (
+          < AnimatedLoader/>
+        )
+      } else if ((!dashboardOptimizationValue && !userToken1)) {
         return (
           <PercentageLoader percentage1={d} />
         )
