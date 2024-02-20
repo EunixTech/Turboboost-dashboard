70dce9b692e68c18c888305406fc33b8746f2821
themanmohan <gitmail223344@gmail.com>
2024-02-20 22:56:00 +0530

    adding Affiliates link

--- src/layouts/index/navbar/index.jsx
+++ src/layouts/index/navbar/index.jsx
@@ -31,7 +31,7 @@ const Prompt = ({setOpen}) => {
   const websiteURL = useSelector((state) => state.toggles?.websiteURL);
 
   const openShopHandler = () => {
-
+    console.log("websiteURL",websiteURL)
     if (websiteURL) {
       window.open(`https://${websiteURL}`, '_blank');
     }
