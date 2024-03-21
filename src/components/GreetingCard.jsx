a2dea8a9ba8ec8cfd43e60dea23bb1a28a12f101
themanmohan <gitmail223344@gmail.com>
2024-03-21 23:59:36 +0530

    fixing intercom issue

--- src/components/GreetingCard.jsx
+++ src/components/GreetingCard.jsx
@@ -60,8 +60,8 @@ export default function GreetingCard() {
                   app_id: "vr8qka5j",
                   name: full_name,
                   email: email_address, // the email for your user
-                  user_id: "65cc86a2d454e8c9c83f2d6e", // a UUID for your user
-                  user_hash: "65cc7a6d881b39eee9da4daac3768f18979528af0cdffda64ddb0a465b00fde4" // an Identity Verification user hash for your user
+                  user_id: user?.id, // a UUID for your user
+                  user_hash: resJSON?.hash // an Identity Verification user hash for your user
                 });
                 dispatch(setToggle({ key: "websiteURL", value: websiteURL }));
 
