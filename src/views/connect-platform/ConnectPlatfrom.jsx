167d107236b5552c51cb555dd2c7a8def1776c94
themanmohan <gitmail223344@gmail.com>
2024-04-15 16:51:52 +0530

    fix: fixing cors issue

--- src/views/connect-platform/ConnectPlatfrom.jsx
+++ src/views/connect-platform/ConnectPlatfrom.jsx
@@ -7,6 +7,7 @@ import { toast } from "react-toastify";
 import { planMockData, planDetail } from "../../utils/constant";
 import FormikInput from "../../components/forms/FormikInput";
 import appURLs from '../../appURL';
+import axio from "axios"
 
 const validationSchema = Yup.object().shape({
   siteURL: Yup.string()
